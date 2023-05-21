ba7d1ac046ca5916d72e4d3182e46fcf8291abc0
Николай <colt92@inbox.ru>
2023-05-21 21:08:40 +0500

    Initial commit

---
+++ src/main/java/com/example/notificationbot/NotificationBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.notificationbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotificationBotApplication.class, args);
+    }
+
+}
